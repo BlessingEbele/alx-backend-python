e4ceba1e96b532ba243d9038ec2c4f18772df64d
blessingebele <blessmart.com@gmail.com>
2025-07-17 19:48:35 +0100

    serializers.py file cfreated

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import CustomUser, Message, Conversation
+
+# 1. User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number']
+
+# 2. Message Serializer
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['message_id', 'conversation', 'sender', 'message_body', 'sent_at']
+
+# 3. Conversation Serializer with nested participants and messages
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'created_at', 'messages']
