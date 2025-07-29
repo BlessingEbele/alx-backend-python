eb9778ffa1a85dd78b76e66a45580d6e9fc6c4d6
blessingebele <blessmart.com@gmail.com>
2025-07-29 19:22:45 +0100

    task 5

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
 from .views import conversation_detail, reply_to_message, unread_messages_view
-from .views import conversation_detail_view, unread_messages
+from .views import conversation_detail_view #unread_messages
 
 urlpatterns = [
     path('messages/<int:message_id>/', views.message_detail, name='message_detail'),
     path('delete_account/', views.delete_user, name='delete_account'),
     path('conversations/<int:conversation_id>/', conversation_detail, name='conversation_detail'),
     path('conversations/<int:conversation_id>/reply/<int:parent_message_id>/', reply_to_message, name='reply_to_message'),
-    path('messages/unread/', unread_messages, name='unread_messages'), 
+    #path('messages/unread/', unread_messages, name='unread_messages'), 
     path('conversation/<int:conversation_id>/', conversation_detail_view, name='conversation_detail'),
     #path('messages/unread/', unread_messages_view, name='unread_messages'),
 ]
